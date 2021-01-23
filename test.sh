62e15ebc1772836d28c373e63645faf3ac66f039
shiitakealien <k165016@gmail.com>
2021-01-23 23:35:28 +0900

    整数1つをコンパイルするコンパイラを作成

---
+++ test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+assert() {
+    expected="$1"
+    input="$2"
+
+    ./jcc "$input" > tmp.s
+    cc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+
+    if [ "$actual" = "$expected" ]; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expetced, but got $actual"
+    fi
+}
+
+assert 0 0
+assert 42 42
+
+echo OK
